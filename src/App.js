ea986202830cbe27403543b9a4af111538d9e713
Juan Diego <diego_suarez@outlook.com>
2020-06-01 15:51:30 -0500

    Se crea componente Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Header from './components/Header';
 
 function App() {
-  return <h1>¡Works!</h1>;
+  return (
+    <Fragment>
+      <Header title="Weather Application" />
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">1</div>
+            <div className="col m6 s12">2</div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
 }
 
 export default App;
